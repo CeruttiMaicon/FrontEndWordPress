665d122709f6705a4f471fd793a56c31df9b18b6
Maicon Cerutti <maicon@litelims.com>
2018-05-02 13:02:27 -0300

    Adicionado mais paginas e imagens para a pagina de Champions

--- index.php
+++ index.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 
+
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Um titulo</title>
+    <title>League of Legends</title>
     <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <body>
-<!-- Start Navigation Bar -->
-<div class="mobile-menu">
-    <i class="fa fa-bars fa-3x js-menu-icon"></i>
-</div>
-<nav class="navbar js-navbar">
-    <ul class="menu">
-        <li>
-            <a href="index.php">Home </a>
-        </li>
-        <li>
-            <a href="#">Champions</a>
-        </li>
-        <li>
-            <a href="#">Runes</a>
-        </li>
-        <li>
-            <a href="#">Masteries</a>
-        </li>
-        <li>
-            <a href="#">Women</a>
-        </li>
-    </ul>
-</nav>
+
+        <!-- Inicio Navegação Menu -->
+    <nav class="navbar js-navbar">
+        <ul class="menu">
+            <li>
+                <a href="index.php">Home </a>
+            </li>
+            <li>
+                <a href="champions.php">Champions</a>
+            </li>
+            <li>
+                <a href="runes.php">Runes</a>
+            </li>
+            <li>
+                <a href="masteries.php">Masteries</a>
+            </li>
+            <li>
+                <a href="elos.php">Elos</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- Fim Naveção Menu -->
+
+    <div class="image__3oO avatar__3vJ" data-am-url="//am-a.akamaihd.net/image?f=https%3A%2F%2Fddragon.leagueoflegends.com%2Fcdn%2Fimg%2Fchampion%2Fsplash%2FAatrox_0.jpg&amp;resize=:475" style="background-image: url(&quot;//am-a.akamaihd.net/image?f=https%3A%2F%2Fddragon.leagueoflegends.com%2Fcdn%2Fimg%2Fchampion%2Fsplash%2FAatrox_0.jpg&amp;resize=:475&quot;); background-position: 75.0617% 16.318%; opacity: 1;"></div>
 </body>
-<!-- End Navigation Bar -->
