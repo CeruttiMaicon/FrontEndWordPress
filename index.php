05b524ce313bb5042044c18f3d58bfc8a90f9e9c
Maicon Cerutti <maicon@litelims.com>
2018-05-02 13:13:00 -0300

    removido do index o link para as masteries

--- index.php
+++ index.php
@@ -23,14 +23,11 @@
                 <a href="runes.php">Runes</a>
             </li>
             <li>
-                <a href="masteries.php">Masteries</a>
-            </li>
-            <li>
                 <a href="elos.php">Elos</a>
             </li>
         </ul>
     </nav>
     <!-- Fim Naveção Menu -->
 
-    <div class="image__3oO avatar__3vJ" data-am-url="//am-a.akamaihd.net/image?f=https%3A%2F%2Fddragon.leagueoflegends.com%2Fcdn%2Fimg%2Fchampion%2Fsplash%2FAatrox_0.jpg&amp;resize=:475" style="background-image: url(&quot;//am-a.akamaihd.net/image?f=https%3A%2F%2Fddragon.leagueoflegends.com%2Fcdn%2Fimg%2Fchampion%2Fsplash%2FAatrox_0.jpg&amp;resize=:475&quot;); background-position: 75.0617% 16.318%; opacity: 1;"></div>
+
 </body>
